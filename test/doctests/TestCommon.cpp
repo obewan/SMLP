222aca661761ce901361b7f4de38a1ac583fbb40
obewan <damien.balima@free.fr>
2024-02-16 14:40:29 +0100

    fix: unit test

--- test/doctests/TestCommon.cpp
+++ test/doctests/TestCommon.cpp
@@ -42,43 +42,49 @@ TEST_CASE("Testing the Common class") {
     CHECK(result.isSuccess() == false);
     CHECK(result.message() == "Failure");
     CHECK(result.json() ==
-          "{\"code\":1,\"data\":\"\",\"message\":\"Failure\"}");
+          "{\"action\":\"\",\"code\":1,\"data\":\"\",\"message\":\"Failure\"}");
 
     result.code = smlp::make_error_code(ErrorCode::Success);
+    result.action = smlp::getModeStr(EMode::Predict);
     CHECK(result.isSuccess() == true);
     CHECK(result.message() == "Success");
-    CHECK(result.json() ==
-          "{\"code\":0,\"data\":\"\",\"message\":\"Success\"}");
+    CHECK(result.json() == "{\"action\":\"Predict\",\"code\":0,\"data\":\"\","
+                           "\"message\":\"Success\"}");
 
     result.code = smlp::make_error_code(ErrorCode::OK);
+    result.action = smlp::getModeStr(EMode::TestOnly);
     result.data = R"({"test":"ok"})";
     CHECK(result.isSuccess() == true);
     CHECK(result.message() == "OK");
-    CHECK(result.json() ==
-          R"({"code":200,"data":"{\"test\":\"ok\"}","message":"OK"})");
+    CHECK(
+        result.json() ==
+        R"({"action":"TestOnly","code":200,"data":"{\"test\":\"ok\"}","message":"OK"})");
 
     result.code = smlp::make_error_code(ErrorCode::BadRequest);
+    result.action = "";
     result.data = R"({"test":"badrequest"})";
     CHECK(result.isSuccess() == false);
     CHECK(result.message() == "Bad Request");
     CHECK(
         result.json() ==
-        R"({"code":400,"data":"{\"test\":\"badrequest\"}","message":"Bad Request"})");
+        R"({"action":"","code":400,"data":"{\"test\":\"badrequest\"}","message":"Bad Request"})");
 
     result.code = smlp::make_error_code(ErrorCode::InternalServerError);
+    result.action = "";
     result.data = R"({"test":"ISE"})";
     CHECK(result.isSuccess() == false);
     CHECK(result.message() == "Internal Server Error");
     CHECK(
         result.json() ==
-        R"({"code":500,"data":"{\"test\":\"ISE\"}","message":"Internal Server Error"})");
+        R"({"action":"","code":500,"data":"{\"test\":\"ISE\"}","message":"Internal Server Error"})");
 
     result.code = std::make_error_code(std::errc::file_exists);
+    result.action = "";
     result.data = R"({"test":"file exists"})";
     CHECK(result.isSuccess() == false);
     CHECK(result.message() == "File exists");
     CHECK(
         result.json() ==
-        R"({"code":17,"data":"{\"test\":\"file exists\"}","message":"File exists"})");
+        R"({"action":"","code":17,"data":"{\"test\":\"file exists\"}","message":"File exists"})");
   }
 }
