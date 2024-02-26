2bdfca6adccf4f7a4f8e54f0f49d0ed9790c3542
obewan <damien.balima@free.fr>
2024-02-26 17:06:28 +0100

    test: add some tests and some cleaning.

--- test/doctests/TestManager.cpp
+++ test/doctests/TestManager.cpp
@@ -83,20 +83,14 @@ TEST_CASE("Testing the Manager class") {
 
   SUBCASE("Test file testing") { CHECK_NOTHROW(manager.test()); }
 
-  SUBCASE("Test run mode") {
+  SUBCASE("Test run mode (FILE)") {
     manager.app_params.network_to_import = "../data/testModel.json";
     manager.app_params.data_file = "../data/test_file.csv";
+    manager.app_params.verbose = true;
     CHECK(std::filesystem::exists(manager.app_params.network_to_import));
     CHECK(std::filesystem::exists(manager.app_params.data_file));
     CHECK_NOTHROW(manager.importOrBuildNetwork());
     manager.app_params.input = EInput::File;
-
-    // for (auto mode :
-    //      {EMode::TestOnly, EMode::TrainOnly, EMode::TrainTestMonitored,
-    //       EMode::TrainThenTest, EMode::Predict}) {
-    //   manager.app_params.mode = mode;
-    //   CHECK_NOTHROW(manager.runMode());
-    // }
     manager.app_params.mode = EMode::Predict;
     CHECK_NOTHROW(manager.runMode());
     manager.app_params.mode = EMode::TestOnly;
@@ -107,14 +101,60 @@ TEST_CASE("Testing the Manager class") {
     CHECK_NOTHROW(manager.runMode());
     manager.app_params.mode = EMode::TrainThenTest;
     CHECK_NOTHROW(manager.runMode());
+    manager.app_params.verbose = false;
   }
 
-  SUBCASE("Test should export network") {
+  SUBCASE("Test run mode (STDIN)") {
+    // Save the original buffer
+    std::streambuf *cin_buffer = std::cin.rdbuf();
+    manager.app_params.network_to_import = "../data/testModel.json";
+    CHECK(std::filesystem::exists(manager.app_params.network_to_import));
+    CHECK_NOTHROW(manager.importOrBuildNetwork());
+    manager.app_params.input = EInput::Stdin;
+    manager.app_params.verbose = true;
+
+    std::string inputData =
+        "0,0.08,0.43,0.90,0.42,1.00,0.62,0.33,0.38,0.10,0.07,0.00,0.00,0.38,"
+        "0.00,0.00,1.00,0.92,0.00,1.00,0.00\n"
+        "1,0.01,0.57,0.90,0.25,1.00,0.00,0.67,0.92,0.09,0.02,0.00, "
+        "0.00,0.62,0.00,0.00,1.00,0.92,0.00,1.00,0.00\n";
+    std::stringstream inputDataStream(inputData);
+
+    // Redirect std::cin
+    std::cin.rdbuf(inputDataStream.rdbuf());
+
+    manager.app_params.mode = EMode::Predict;
+    CHECK_NOTHROW(manager.runMode());
+    manager.app_params.mode = EMode::TestOnly;
+    CHECK_NOTHROW(manager.runMode());
+    manager.app_params.mode = EMode::TrainOnly;
+    CHECK_NOTHROW(manager.runMode());
+    manager.app_params.mode = EMode::TrainTestMonitored;
+    CHECK_NOTHROW(manager.runMode());
+    manager.app_params.mode = EMode::TrainThenTest;
+    CHECK_NOTHROW(manager.runMode());
+    // Restore std::cin to normal after the test
+    std::cin.rdbuf(cin_buffer);
+    manager.app_params.verbose = false;
+  }
+
+  SUBCASE("Test export network") {
     manager.app_params.network_to_export = "";
     CHECK(manager.shouldExportNetwork() == false);
-    manager.app_params.network_to_export = "fakeFile";
+    manager.app_params.network_to_export = "fakeFile.json";
     manager.app_params.mode = EMode::TrainOnly;
     CHECK(manager.shouldExportNetwork() == true);
+    CHECK(!std::filesystem::exists(manager.app_params.network_to_export));
+    manager.app_params.network_to_import = "../data/testModel.json";
+    CHECK(std::filesystem::exists(manager.app_params.network_to_import));
+    CHECK_NOTHROW(manager.importOrBuildNetwork());
+    CHECK_NOTHROW(manager.exportNetwork());
+    CHECK(std::filesystem::exists("fakeFile.json"));
+    CHECK(std::filesystem::exists("fakeFile.csv"));
+    std::filesystem::remove("fakeFile.json");
+    std::filesystem::remove("fakeFile.csv");
+    CHECK(!std::filesystem::exists("fakeFile.json"));
+    CHECK(!std::filesystem::exists("fakeFile.csv"));
   }
 
   SUBCASE("Test procesTCPClient") {
