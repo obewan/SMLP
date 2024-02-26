eab5cc1c783d35310e95455011b82f0827a58f00
obewan <damien.balima@free.fr>
2024-02-26 19:42:38 +0100

    docs: update

--- data/mushroom/format.sh
+++ data/mushroom/format.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-# Format for SMLP
+# Format and normalize data for SMLP
 src=mushroom_data_orig.csv
 dest=mushroom_data.csv
 formater=mushroom_formater.awk
 
 echo "Formatting $src to $dest, please wait..."
+
+# Send the file without its header to the formater and write the dest file
 tail -n +2 $src | awk -f $formater > $dest
+
+# Shuffle the lines of the dest file for better training
 shuf $dest -o $dest
+
 echo "done."
